4d3112ba4bafc45eaaeae315df9ec550e3b519ca
zqsd <17475349+zqsd1@users.noreply.github.com>
2026-05-17 17:18:09 +0200

    boring

--- hassio-access-point-dev/run.sh
+++ hassio-access-point-dev/run.sh
@@ -128,14 +128,16 @@ config_nm(){
         802-11-wireless.band bg \
         802-11-wireless.channel "$CHANNEL"
 
+    nmcli connection modify hassio-access-point \
+    -wifi-sec.proto \
+    -wifi-sec.pairwise \
+    -wifi-sec.group
     bashio::log.info "set access point security"
     nmcli connection modify hassio-access-point \
         wifi-sec.key-mgmt wpa-psk \
         wifi-sec.psk "$WPA_PASSPHRASE" \
           802-11-wireless-security.pmf 0
-    #     wifi-sec.proto rsn \
-    #     wifi-sec.pairwise ccmp \
-    #     wifi-sec.group ccmp
+
 
     bashio::log.info "set access point network config"
     nmcli connection modify hassio-access-point \
