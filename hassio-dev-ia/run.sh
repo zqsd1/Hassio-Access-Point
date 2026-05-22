f8443f0d2488685ea7bed0377c8f9cdacf525de0
zqsd <17475349+zqsd1@users.noreply.github.com>
2026-05-22 14:33:51 +0200

    exec and background conflict

--- hassio-dev-ia/run.sh
+++ hassio-dev-ia/run.sh
@@ -228,7 +228,7 @@ logger "## Starting hostapd" 1
 if [ "$DEBUG" -gt 1 ]; then
 	exec hostapd -dd "$RUN_HOSTAPD"
 else
-	exec hostapd "$RUN_HOSTAPD" &
+	hostapd "$RUN_HOSTAPD" &
 fi
 
 tcpdump -i "${INTERFACE}"
